6d88744fa38b1fa1b087a10c405921edd2094b85
Randy Tate <randytatejr@gmail.com>
2019-09-24 21:27:23 -0500

    adding listeners to buttons to print

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 import cheese from './components/cheese.js'
+import order from './components/order.js'
 
 // init function goes in the main
 const init = () => {
     cheese.printCheeseOptions();
-}
+    order.printOrderButton();
+};
 
 init();
